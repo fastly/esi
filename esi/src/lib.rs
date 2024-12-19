2b59e00e19ec6fe3fbdaf8082b58e62c817def6e
Tyler McMullen <tyler@fastly.com>
2024-12-18 16:50:20 -0800

    Getting there. Correctly-ish identify HTML (and ignore it) and pull out esi:vars and recognize basic interpolation.

--- esi/src/lib.rs
+++ esi/src/lib.rs
@@ -7,6 +7,7 @@ mod expression;
 mod functions;
 mod new_parse;
 mod parse;
+mod parser_types;
 
 use document::{FetchState, Task};
 use expression::{evaluate_expression, try_evaluate_interpolated, EvalContext};
