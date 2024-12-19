73369c5a5df3be49d80b7661b3429934f8c2cb7f
Tyler McMullen <tyler@fastly.com>
2024-12-19 14:23:24 -0800

    All of the rest of the esi tags we currently support.

--- esi/src/parser_types.rs
+++ esi/src/parser_types.rs
@@ -1,20 +1,18 @@
-#[derive(Debug)]
-pub enum Tag {
-    Vars,
-    Include,
-    Text,
-    Choose,
-    When,
-    Otherwise,
-    Try,
-    Attempt,
-    Except,
+#[derive(Debug, Clone)]
+pub enum Tag<'a> {
+    Include(Vec<(&'a str, &'a str)>),
+    Choose(Vec<Chunk<'a>>, Option<Vec<Chunk<'a>>>),
+    When(Vec<(&'a str, &'a str)>, Vec<Chunk<'a>>),
+    Otherwise(Vec<Chunk<'a>>),
+    Try(Vec<Vec<Chunk<'a>>>, Option<Vec<Chunk<'a>>>),
+    Attempt(Vec<Chunk<'a>>),
+    Except(Vec<Chunk<'a>>),
+    Assign(Vec<(&'a str, &'a str)>, Option<Vec<Chunk<'a>>>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Chunk<'a> {
-    EsiStartTag(Tag, Vec<(&'a str, &'a str)>),
-    EsiEndTag(Tag),
+    Esi(Tag<'a>),
     Expr(&'a str),
     Html(&'a str),
     Text(&'a str),
